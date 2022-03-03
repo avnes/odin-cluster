6fff76c286f802dd7f9765e53f61ce09b21685a4
Audun Nes <audun.nes@gmail.com>
2022-03-03 13:03:00 +0100

    Add Ansible to install CLI tool
    Signed-off-by: Audun Nes <audun.nes@gmail.com>

--- README.md
+++ README.md
@@ -4,5 +4,12 @@ Script for setting up a Kubernetes cluster running k0s
 
 ## Requirements
 
-1. Virtual machines have been created using <https://github.com/avnes/terraform-libvirt-vm>
-2. Ansible installed on the host.
+- Virtual machines have been created using <https://github.com/avnes/terraform-libvirt-vm>
+
+## Run instructions
+
+```bash
+bin/01_install_cli_tools.sh
+
+bin/02_create_k8s_cluster.sh
+```
---
+++ bin/01_install_cli_tools.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Make sure the script works regardless of which directory it is called from
+cd "${0%/*}/.."
+
+printf "\n\n############### Installing Linux software requirements ###############\n"
+sudo dnf install -y python curl
+
+printf "\n\n############### Installing Poetry ###############\n"
+curl -sSL https://install.python-poetry.org | python3 -
+PATH=$HOME/.local/bin:$PATH; export PATH
+
+printf "\n\n############### Installing Ansible ###############\n"
+poetry install
+
+printf "\n\n############### Installing Ansible roles ###############\n"
+poetry run ansible-galaxy role install --roles-path ansible/roles --role-file ansible/requirements.yaml
+
+printf "\n\n############### Installing CLI tools ###############\n"
+poetry run ansible-playbook -i ansible/inventory/hosts.yaml ansible/install_cli_tools.yaml
