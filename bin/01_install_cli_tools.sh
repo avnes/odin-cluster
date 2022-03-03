307e1d2e87dca67e26c89c03694ddd35eb464c1a
Audun Nes <audun.nes@gmail.com>
2022-03-03 13:26:21 +0100

    Use latest version of Helm and use ZSH as my shell
    Signed-off-by: Audun Nes <audun.nes@gmail.com>

--- bin/01_install_cli_tools.sh
+++ bin/01_install_cli_tools.sh
@@ -17,4 +17,4 @@ printf "\n\n############### Installing Ansible roles ###############\n"
 poetry run ansible-galaxy role install --roles-path ansible/roles --role-file ansible/requirements.yaml
 
 printf "\n\n############### Installing CLI tools ###############\n"
-poetry run ansible-playbook -i ansible/inventory/hosts.yaml ansible/install_cli_tools.yaml
+poetry run ansible-playbook --inventory ansible/inventory/hosts.yaml ansible/install_cli_tools.yaml
