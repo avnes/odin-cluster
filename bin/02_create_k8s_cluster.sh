9949de2c39bc500e55277160b9baf6d04b5c2480
Audun V. Nes <audun.nes@gmail.com>
2022-08-19 20:45:25 +0200

    Update 02_create_k8s_cluster.sh

--- bin/02_create_k8s_cluster.sh
+++ bin/02_create_k8s_cluster.sh
@@ -14,7 +14,7 @@ unset KUBECONFIG
 if [[ $K0S_CLUSTER == 'valyria' ]]; then
     cd ~/git/k0s-vm
     VM_PRIVATE_KEY=$(terraform output --raw k0s_vm_ssh_private_key_filename)
-    IFS=':' VM_NODES='10.0.1.20 10.0.1.21 10.0.1.22 10.0.1.23'
+    IFS=' ' VM_NODES='10.0.1.20 10.0.1.21 10.0.1.22 10.0.1.23'
 elif [[ $K0S_CLUSTER == 'dragonstone' ]]; then
     cd ~/git/terraform-libvirt-vm
     VM_PRIVATE_KEY=$(terraform output --raw ssh_private_key_filename)
